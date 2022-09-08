61109987fd1be8e0e03d3aaef0ab7d467f47e66a
Linssssssss <wanjikulinsey@gmail.com>
2022-09-08 06:20:20 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-int main(void)
-{
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
 
-	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
